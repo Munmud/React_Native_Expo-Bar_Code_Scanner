e65922b430848afe23632890979d2bb30b69bd26
Moontasir Mahmood <moontasir042@gmail.com>
2022-10-18 19:30:52 +0600

    bar code added

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [scannedText, setScannedTest] = useState("");
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    // setScanned(true);
+
+    // alert(`type : ${type} and data : ${data} `);
+    setScannedTest(`type : ${type} and data : ${data} `);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {/* {scanned && ( */}
+      {/* //{" "} */}
+      {/* <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} /> */}
+      <View style={styles.whiteBackgroundStyle}>
+        <Text style={styles.textStyle}>{scannedText}</Text>
+      </View>
+      {/* )} */}
     </View>
   );
 }
@@ -13,8 +50,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  whiteBackgroundStyle: {
+    backgroundColor: "#ecf0f1",
+  },
+  textStyle: {
+    justifyContent: "center",
+    fontWeight: "bold",
+    padding: 16,
+    color: "red",
   },
 });
---
+++ readme.md
@@ -0,0 +1,3 @@
+- `npx create-expo-app p2_expo_doc`
+- `npx expo install expo-camera`
+- `npx expo install expo-barcode-scanner`
